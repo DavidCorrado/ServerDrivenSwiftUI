356cb914e701947a7224527f8b37ee0f90b37985
david.corrado <david.corrado@well.co>
2022-07-08 17:12:41 -0500

    Fixing display issues

--- ServerDriveniOS/views/Entry.swift
+++ ServerDriveniOS/views/Entry.swift
@@ -17,7 +17,7 @@ func entry() -> ServerCard {
     return ServerCard(items:[
         ServerColumn(modifier: ServerModifier(paddingStart: 20, paddingEnd: 20, paddingTop: 24, paddingBottom: 24),
                      items:[
-                        ServerRow(alignment: .CENTER,items: [ServerImage(drawableRes: "BP", modifier: ServerModifier(paddingEnd: 7, width: 18, height: 18)), ServerText(text: "Blood Pressure", size: 14, weight: "500"),ServerSpacer(),ServerText(text: "Nov 1", size: 12, weight:"500"), ServerText(text: "8:31", weight: "700")]),
+                        ServerRow(alignment: .CENTER,items: [ServerImage(drawableRes: "BP", modifier: ServerModifier(paddingEnd: 7, width: 18, height: 18)), ServerText(text: "Blood Pressure", size: 14, weight: "500"),ServerSpacer(),ServerText(text: "Nov 1 ", size: 12, weight:"600"), ServerText(text: "8:31", size: 12, weight: "700")]),
                         ServerText(text: "123/82", size: 36, color:"#002F8C", weight: "300", modifier: ServerModifier(paddingTop: 21, paddingBottom: 15)),
                         ServerRow(alignment: .CENTER, items: [ServerImage(drawableRes: "Running", modifier: ServerModifier(paddingEnd: 8, width: 20, height: 20)),ServerText(text: "Bubble TBD", modifier: ServerModifier(paddingEnd: 8)), ServerText(text: "Per AHA guidelines", size: 12, weight: "500")])
                      ] )]

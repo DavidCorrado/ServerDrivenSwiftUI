b0bd938c1bdf2e12ad3d108c780181636341f943
david.corrado <david.corrado@well.co>
2022-07-08 15:23:01 -0500

    Text

---
+++ ServerDriveniOS/Extensions/ColorExtension.swift
@@ -0,0 +1,40 @@
+//
+//  ColorExtension.swift
+//  ServerDriveniOS
+//
+//  Created by David Corrado on 7/8/22.
+//
+
+import Foundation
+import UIKit
+
+extension UIColor {
+    
+    convenience init(withHex: String, alpha: CGFloat = 1.0) {
+        var hexString = withHex
+        if hexString.isEmpty {
+            hexString = "F5F5F5"
+        }
+        
+        var cString: String = hexString.trimmingCharacters(in: .whitespacesAndNewlines).uppercased()
+        
+        if cString.hasPrefix("#") {
+            cString.remove(at: cString.startIndex)
+        }
+        
+        // Default to white on bad data
+        if cString.count != 6 {
+            hexString = "F5F5F5"
+        }
+        
+        var rgbValue: UInt64 = 0
+        Scanner(string: cString).scanHexInt64(&rgbValue)
+        
+        self.init(
+            red: CGFloat((rgbValue & 0xFF0000) >> 16) / 255.0,
+            green: CGFloat((rgbValue & 0x00FF00) >> 8) / 255.0,
+            blue: CGFloat(rgbValue & 0x0000FF) / 255.0,
+            alpha: alpha
+        )
+    }
+}
--- ServerDriveniOS/ui/sd/view/SDText.swift
+++ ServerDriveniOS/ui/sd/view/SDText.swift
@@ -12,10 +12,14 @@ struct SDText: View {
     var serverText: ServerText
     var body: some View {
         Text(serverText.text)
+            .foregroundColor(Color(UIColor.init(withHex: serverText.color ?? "#4C5870")))
             .fontWeight(serverText.fontWeight())
             .font(.system(size: CGFloat(serverText.size ?? 16)))
-            .modifyIf(serverText.decoration.contains(DecorationType.STRIKETHROUGH), transform: {
+            .modifyIf(serverText.decoration.contains(.STRIKETHROUGH), transform: {
                 $0.strikethrough(true)
+            })
+            .modifyIf(serverText.style == .ITALIC, transform: {
+                ($0 as? Text)?.italic()
             })
             .modifyIf(serverText.isHeading ?? false, transform: {
                         $0.accessibilityAddTraits(.isHeader)

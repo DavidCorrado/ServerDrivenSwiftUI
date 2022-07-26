37c845972a86dc501a3ccf3cf67ad595c9b70e7d
firots <firat.ozkms@gmail.com>
2022-07-26 18:22:09 +0300

    Re-indent external code

--- ServerDriveniOS/views/SingleAxisGeometryReader.swift
+++ ServerDriveniOS/views/SingleAxisGeometryReader.swift
@@ -9,27 +9,24 @@ import SwiftUI
 
 // This is working by observing size multiple times
 // May need a performance-wise inspection
-struct SingleAxisGeometryReader<Content: View>: View
-{
-    private struct SizeKey: PreferenceKey
-    {
+struct SingleAxisGeometryReader<Content: View>: View {
+    private struct SizeKey: PreferenceKey {
         static var defaultValue: CGFloat { 10 }
         static func reduce(value: inout CGFloat, nextValue: () -> CGFloat)
         {
             value = max(value, nextValue())
         }
     }
-
+    
     @State private var size: CGFloat = SizeKey.defaultValue
-
+    
     var axis: Axis = .horizontal
     let content: (CGFloat)->Content
-
-    var body: some View
-    {
+    
+    var body: some View {
         content(size)
             .background(GeometryReader
-            {
+                        {
                 proxy in
                 Color.clear.preference(key: SizeKey.self, value: axis == .horizontal ? proxy.size.width : proxy.size.height)
             })

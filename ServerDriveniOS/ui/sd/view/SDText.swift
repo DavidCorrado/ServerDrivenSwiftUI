34399b0b11041b355a35429899ca5977e5d4f5ea
firots <firat.ozkms@gmail.com>
2022-08-19 18:02:22 +0300

    Remove test code

--- ServerDriveniOS/ui/sd/view/SDText.swift
+++ ServerDriveniOS/ui/sd/view/SDText.swift
@@ -20,23 +20,20 @@ struct SDText: View {
     @Environment(\.legibilityWeight) var legibilityWeight
     
     var body: some View {
-        AnyView {
-            Text(serverText.text)
-                .foregroundColor(Color(UIColor(withHex: serverText.color ?? "#4C5870")))
-                .fontWeight(serverText.fontWeight())
-                .font(.system(size: UIFontMetrics.default.scaledValue(for: CGFloat(serverText.size ?? 16))))
-                .modifyIf(serverText.decoration.contains(.STRIKETHROUGH), transform: {
-                    $0.strikethrough(true)
-                })
-                .modifyIf(serverText.style == .ITALIC, transform: {
-                    ($0 as? Text)?.italic()
-                })
-                .modifyIf(serverText.isHeading ?? false, transform: {
-                    $0.accessibilityAddTraits(.isHeader)
-                })
-                .multilineTextAlignment(serverText.textAlignment?.alignment ?? .leading)
-                .serverModifier(serverView: serverText, parentWeightDirection: parentWeightDirection, parentSize: parentSize, parentTotalWeight: parentTotalWeight, nestedInVerticalLayout: nestedInVerticalLayout, nestedInHorizontalLayout: nestedInHorizontalLayout)
-        }
-
+        Text(serverText.text)
+            .foregroundColor(Color(UIColor(withHex: serverText.color ?? "#4C5870")))
+            .fontWeight(serverText.fontWeight())
+            .font(.system(size: UIFontMetrics.default.scaledValue(for: CGFloat(serverText.size ?? 16))))
+            .modifyIf(serverText.decoration.contains(.STRIKETHROUGH), transform: {
+                $0.strikethrough(true)
+            })
+            .modifyIf(serverText.style == .ITALIC, transform: {
+                ($0 as? Text)?.italic()
+            })
+            .modifyIf(serverText.isHeading ?? false, transform: {
+                $0.accessibilityAddTraits(.isHeader)
+            })
+            .multilineTextAlignment(serverText.textAlignment?.alignment ?? .leading)
+            .serverModifier(serverView: serverText, parentWeightDirection: parentWeightDirection, parentSize: parentSize, parentTotalWeight: parentTotalWeight, nestedInVerticalLayout: nestedInVerticalLayout, nestedInHorizontalLayout: nestedInHorizontalLayout)
     }
 }
